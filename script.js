d5b9abdd7ead00f30972209c980d7b8360a61bac
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-03-27 20:53:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //your JS code here. If required.
 function createPromise(condition,delay,resState,rejectState){
 	return new Promise(resolve,reject){
+		let startTime = Date.now();
 		setTimeout(()=>{
+			let endTime = Date.now();
+			let timeTaken = (endTime-startTime)/1000
 			if(condition){
-				resolve(resState);
+				resolve({resState,timeTaken});
 			}else{
 				reject(rejectState);
 			}
@@ -16,24 +19,16 @@ let p2 = createPromise(true,2000,"Promise 2","rejected");
 let p3 = createPromise(true,2300,"Promise 3","rejected");
 
 let comb = Promise.all([p1,p2.p3]);
-let loading = document.getElementById("load");
-loading.classList.add("hide");
+comb.then((values)=>{
+	let loading = document.getElementById("load");
+	loading.classList.add("hide");
 
-let tablee = document.getElementByClass("operation");
-let row1 = tablee.insertRow(1);
-let col10 = row1.insertCell(0);
-let col11 = row1.insertCell(1);
-col10.innerHTML = p1.then((data)=>return data);
-col11.innerHTML = "1";
-
-let row2 = tablee.insertRow(2);
-let col20 = row1.insertCell(0);
-let col21 = row1.insertCell(1);
-col20.innerHTML = p2.then((data)=>return data);
-col21.innerHTML = "2";
-
-let row3 = tablee.insertRow(3);
-let col30 = row1.insertCell(0);
-let col31 = row1.insertCell(1);
-col30.innerHTML = p3.then((data)=>return data);
-col31.innerHTML = "2.3";
+	let tablee = document.getElementByClass("operation")[0];
+	for(let i=0i;i<values.length;i++){
+		let row = tablee.insertRow[i+1];
+		let col1 = row.insertCell[0];
+		let col2 = row.insertCell[1];
+		col1.innerHTML = values[i].resState;
+		col2.innerHTML = values[i].timeTaken;
+	}
+})

8a0a42a1b21d540019a84911de13d2ea482aaa64
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-03-27 20:57:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function createPromise(condition,delay,resState,rejectState){
-	return new Promise(resolve,reject){
+	return new Promise((resolve,reject)=>{
 		let startTime = Date.now();
 		setTimeout(()=>{
 			let endTime = Date.now();
@@ -11,23 +11,23 @@ function createPromise(condition,delay,resState,rejectState){
 				reject(rejectState);
 			}
 		},delay);
-	}
+	});
 }
 
 let p1 = createPromise(true,1000,"Promise 1","rejected");
 let p2 = createPromise(true,2000,"Promise 2","rejected");
 let p3 = createPromise(true,2300,"Promise 3","rejected");
 
-let comb = Promise.all([p1,p2.p3]);
+let comb = Promise.all([p1,p2,p3]);
 comb.then((values)=>{
 	let loading = document.getElementById("load");
 	loading.classList.add("hide");
 
 	let tablee = document.getElementByClass("operation")[0];
-	for(let i=0i;i<values.length;i++){
-		let row = tablee.insertRow[i+1];
-		let col1 = row.insertCell[0];
-		let col2 = row.insertCell[1];
+	for(let i=0;i<values.length;i++){
+		let row = tablee.insertRow(i+1);
+		let col1 = row.insertCell(0);
+		let col2 = row.insertCell(1);
 		col1.innerHTML = values[i].resState;
 		col2.innerHTML = values[i].timeTaken;
 	}
